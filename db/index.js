cc5ad51dcdeebbf0f5e1be748f29a44e04590cb1
Renan Cidale <renancidale@hotmail.com>
2020-06-25 08:43:36 +0200

    Update projects db location

--- db/index.js
+++ db/index.js
@@ -1,8 +1,9 @@
 import Datastore from "nedb";
+import path from "path";
 
 export default {
   projects: new Datastore({
     autoload: true,
-    filename: "projects.db",
+    filename: path.join(process.cwd(), "projects.db"),
   }),
 };
